34000fde51ff56b5dde7a45cfdc625ef13e04440
Nileshnhe <nehenilesh111@gmail.com>
2026-03-28 22:09:38 +0530

    feat: add user authentication and email verification functionality

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerValidator } from "../validators/auth.validator.js";
-import { register, verifyEmail } from "../controllers/auth.controller.js";
+import { loginValidator, registerValidator } from "../validators/auth.validator.js";
+import { register, verifyEmail, login, getMe } from "../controllers/auth.controller.js";
+import { authUser } from "../middleware/auth.middleware.js";
 const authRouter = Router();
 
 
@@ -12,10 +13,29 @@ const authRouter = Router();
  */
 authRouter.post("/register", registerValidator, register);
 
+/**
+ * @route POST /api/auth/login
+ * @desc Login user and return jwt token
+ * @access Public  
+ * @body {  email, password }
+ */
+authRouter.post("/login", loginValidator, login )
 
 
+/**
+ * @desc Get current logged in user's details
+ * @route GET /api/auth/get-me
+ * @access Private  
+ */
+authRouter.get("/get-me", authUser, getMe)
 
 
+/**
+ * @route GET /api/auth/login
+ * @desc verify user email
+ * @access Public
+ * @query { token }
+ */
 authRouter.get('/verify-email', verifyEmail )
 
 export default authRouter;
